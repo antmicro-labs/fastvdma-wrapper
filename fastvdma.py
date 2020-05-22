cd8f65efcbc464cc83cae33e21a947e1d0d128c5
K. A. Brodzki <kabrodzki@gmail.com>
2020-05-22 11:47:12 +0200

    Add LiteX prefixes

--- fastvdma.py
+++ fastvdma.py
@@ -1,6 +1,8 @@
 from migen import *
+from litex.soc.interconnect.csr import *
+from litex.soc.interconnect import wishbone
 
-class FastVDMA(Module):
+class FastVDMA(Module, AutoCSR):
     def __init__(self, platform):
         platform.add_sources("DMATop.v")
 
@@ -44,43 +46,43 @@ class FastVDMA(Module):
         self.sync_writerSync = Signal()
 
         self.specials += Instance("DMATop",
-                            clock = ClockSignal(),
-                            reset = ~ResetSignal(),
-                            io_control_dat_i   = self.control_dat_i,
-                            io_constrol_dat_o  = self.constrol_dat_o,
-                            io_control_cyc_i   = self.control_cyc_i,
-                            io_control_stb_i   = self.control_stb_i,
-                            io_control_we_i    = self.control_we_i,
-                            io_control_adr_i   = self.control_adr_i,
-                            io_control_sel_i   = self.control_sel_i,
-                            io_control_ack_o   = self.control_ack_o,
-                            io_control_stall_o = self.control_stall_o,
-                            io_control_err_o   = self.control_err_o,
+                            i_clock = ClockSignal(),
+                            i_reset = ~ResetSignal(),
+                            i_io_control_dat_i   = self.control_dat_i,
+                            o_io_constrol_dat_o  = self.constrol_dat_o,
+                            i_io_control_cyc_i   = self.control_cyc_i,
+                            i_io_control_stb_i   = self.control_stb_i,
+                            i_io_control_we_i    = self.control_we_i,
+                            i_io_control_adr_i   = self.control_adr_i,
+                            i_io_control_sel_i   = self.control_sel_i,
+                            o_io_control_ack_o   = self.control_ack_o,
+                            o_io_control_stall_o = self.control_stall_o,
+                            o_io_control_err_o   = self.control_err_o,
                             
-                            io_read_dat_i    = self.read_dat_i,
-                            io_read_dat_o    = self.read_dat_o,
-                            io_read_cyc_o    = self.read_cyc_o,
-                            io_read_stb_o    = self.read_stb_o,
-                            io_read_we_o     = self.read_we_o,
-                            io_read_adr_o    = self.read_adr_o,
-                            io_read_sel_o    = self.read_sel_o,
-                            io_read_ack_i    = self.read_ack_i,
-                            io_read_stall_i  = self.read_stall_i,
-                            io_read_err_i    = self.read_err_i,
+                            i_io_read_dat_i    = self.read_dat_i,
+                            o_io_read_dat_o    = self.read_dat_o,
+                            o_io_read_cyc_o    = self.read_cyc_o,
+                            o_io_read_stb_o    = self.read_stb_o,
+                            o_io_read_we_o     = self.read_we_o,
+                            o_io_read_adr_o    = self.read_adr_o,
+                            o_io_read_sel_o    = self.read_sel_o,
+                            i_io_read_ack_i    = self.read_ack_i,
+                            i_io_read_stall_i  = self.read_stall_i,
+                            i_io_read_err_i    = self.read_err_i,
 
-                            io_write_dat_i   = self.write_dat_i,
-                            io_write_dat_o   = self.write_dat_o,
-                            io_write_cyc_o   = self.write_cyc_o,
-                            io_write_stb_o   = self.write_stb_o,
-                            io_write_we_o    = self.write_we_o,
-                            io_write_adr_o   = self.write_adr_o,
-                            io_write_sel_o   = self.write_sel_o,
-                            io_write_ack_i   = self.write_ack_i,
-                            io_write_stall_i = self.write_stall_i,
-                            io_write_err_i   = self.write_err_i,
+                            i_io_write_dat_i   = self.write_dat_i,
+                            o_io_write_dat_o   = self.write_dat_o,
+                            o_io_write_cyc_o   = self.write_cyc_o,
+                            o_io_write_stb_o   = self.write_stb_o,
+                            o_io_write_we_o    = self.write_we_o,
+                            o_io_write_adr_o   = self.write_adr_o,
+                            o_io_write_sel_o   = self.write_sel_o,
+                            i_io_write_ack_i   = self.write_ack_i,
+                            i_io_write_stall_i = self.write_stall_i,
+                            i_io_write_err_i   = self.write_err_i,
 
-                            io_irq_readerDone = self.irq_readerDone,
-                            io_irq_writerDone = self.irq_writerDone,
+                            o_io_irq_readerDone = self.irq_readerDone,
+                            o_io_irq_writerDone = self.irq_writerDone,
 
-                            io_sync_readerSync = self.sync_readerSync,
-                            io_sync_writerSync = self.sync_writerSync)
+                            i_io_sync_readerSync = self.sync_readerSync,
+                            i_io_sync_writerSync = self.sync_writerSync)
