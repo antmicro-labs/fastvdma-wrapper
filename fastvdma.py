ceb3c7d73c28af2b9862d6bfb34acd1ff3cbcb1f
K. A. Brodzki <kabrodzki@gmail.com>
2020-05-22 14:41:50 +0200

    Use wishbone interface instead of dummy signals

--- fastvdma.py
+++ fastvdma.py
@@ -1,43 +1,19 @@
 import os
 from migen import *
+from litex.soc.interconnect import wishbone
 
 class FastVDMA(Module):
     def __init__(self, platform):
         platform.add_sources(os.path.abspath(os.path.dirname(__file__)),
                              "DMATop.v")
 
-        self.control_dat_i   = Signal(32)
-        self.control_dat_o   = Signal(32)
-        self.control_cyc_i   = Signal()
-        self.control_stb_i   = Signal()
-        self.control_we_i    = Signal()
-        self.control_adr_i   = Signal(32)
-        self.control_sel_i   = Signal(32)
-        self.control_ack_o   = Signal()
-        self.control_stall_o = Signal()
-        self.control_err_o   = Signal()
+        self.control = control_bus = wishbone.Interface(data_width=32, adr_width=32)
+        self.read    = read_bus    = wishbone.Interface(data_width=32, adr_width=32) 
+        self.write   = write_bus   = wishbone.Interface(data_width=32, adr_width=32)
 
-        self.read_dat_i    = Signal(32)
-        self.read_dat_o    = Signal(32)
-        self.read_cyc_o    = Signal()
-        self.read_stb_o    = Signal()
-        self.read_we_o     = Signal()
-        self.read_adr_o    = Signal(32)
-        self.read_sel_o    = Signal(4)
-        self.read_ack_i    = Signal()
-        self.read_stall_i  = Signal()
-        self.read_err_i    = Signal()
-        
-        self.write_dat_i   = Signal(32)
-        self.write_dat_o   = Signal(32)
-        self.write_cyc_o   = Signal()
-        self.write_stb_o   = Signal()
-        self.write_we_o    = Signal()
-        self.write_adr_o   = Signal(32)
-        self.write_sel_o   = Signal(4)
-        self.write_ack_i   = Signal()
-        self.write_stall_i = Signal()
-        self.write_err_i   = Signal() 
+        self.control_stall = Signal()
+        self.read_stall = Signal()
+        self.write_stall = Signal()
         
         self.irq_readerDone = Signal()
         self.irq_writerDone = Signal()
@@ -48,41 +24,47 @@ class FastVDMA(Module):
         self.specials += Instance("DMATop",
                             i_clock = ClockSignal(),
                             i_reset = ~ResetSignal(),
-                            i_io_control_dat_i   = self.control_dat_i,
-                            o_io_control_dat_o   = self.control_dat_o,
-                            i_io_control_cyc_i   = self.control_cyc_i,
-                            i_io_control_stb_i   = self.control_stb_i,
-                            i_io_control_we_i    = self.control_we_i,
-                            i_io_control_adr_i   = self.control_adr_i,
-                            i_io_control_sel_i   = self.control_sel_i,
-                            o_io_control_ack_o   = self.control_ack_o,
-                            o_io_control_stall_o = self.control_stall_o,
-                            o_io_control_err_o   = self.control_err_o,
-                            
-                            i_io_read_dat_i    = self.read_dat_i,
-                            o_io_read_dat_o    = self.read_dat_o,
-                            o_io_read_cyc_o    = self.read_cyc_o,
-                            o_io_read_stb_o    = self.read_stb_o,
-                            o_io_read_we_o     = self.read_we_o,
-                            o_io_read_adr_o    = self.read_adr_o,
-                            o_io_read_sel_o    = self.read_sel_o,
-                            i_io_read_ack_i    = self.read_ack_i,
-                            i_io_read_stall_i  = self.read_stall_i,
-                            i_io_read_err_i    = self.read_err_i,
-
-                            i_io_write_dat_i   = self.write_dat_i,
-                            o_io_write_dat_o   = self.write_dat_o,
-                            o_io_write_cyc_o   = self.write_cyc_o,
-                            o_io_write_stb_o   = self.write_stb_o,
-                            o_io_write_we_o    = self.write_we_o,
-                            o_io_write_adr_o   = self.write_adr_o,
-                            o_io_write_sel_o   = self.write_sel_o,
-                            i_io_write_ack_i   = self.write_ack_i,
-                            i_io_write_stall_i = self.write_stall_i,
-                            i_io_write_err_i   = self.write_err_i,
+                            # slave
+                            i_io_control_dat_i   = control_bus.dat_w,
+                            o_io_control_dat_o   = control_bus.dat_r,
+                            i_io_control_cyc_i   = control_bus.cyc,
+                            i_io_control_stb_i   = control_bus.stb,
+                            i_io_control_we_i    = control_bus.we,
+                            i_io_control_adr_i   = control_bus.adr,
+                            i_io_control_sel_i   = control_bus.sel,
+                            o_io_control_ack_o   = control_bus.ack,
+                            o_io_control_stall_o = self.control_stall, 
+                            o_io_control_err_o   = control_bus.err,
+                            # master
+                            i_io_read_dat_i    = read_bus.dat_r,
+                            o_io_read_dat_o    = read_bus.dat_w,
+                            o_io_read_cyc_o    = read_bus.cyc,
+                            o_io_read_stb_o    = read_bus.stb,
+                            o_io_read_we_o     = read_bus.we,
+                            o_io_read_adr_o    = read_bus.adr,
+                            o_io_read_sel_o    = read_bus.sel,
+                            i_io_read_ack_i    = read_bus.ack,
+                            i_io_read_stall_i  = self.read_stall,
+                            i_io_read_err_i    = read_bus.err,
+                            # master
+                            i_io_write_dat_i   = write_bus.dat_r,
+                            o_io_write_dat_o   = write_bus.dat_w,
+                            o_io_write_cyc_o   = write_bus.cyc,
+                            o_io_write_stb_o   = write_bus.stb,
+                            o_io_write_we_o    = write_bus.we,
+                            o_io_write_adr_o   = write_bus.adr,
+                            o_io_write_sel_o   = write_bus.sel,
+                            i_io_write_ack_i   = write_bus.ack,
+                            i_io_write_stall_i = self.write_stall,
+                            i_io_write_err_i   = write_bus.err,
 
                             o_io_irq_readerDone = self.irq_readerDone,
                             o_io_irq_writerDone = self.irq_writerDone,
 
                             i_io_sync_readerSync = self.sync_readerSync,
                             i_io_sync_writerSync = self.sync_writerSync)
+        self.comb += [
+            self.control_stall.eq(0),
+            self.read_stall.eq(0),
+            self.write_stall.eq(0)
+        ]
